63957eb2e5fc7e487eb4c8e0863262440bd467d1
bhokaresneha <bhokaresneha20@gmail.com>
2022-06-24 08:17:46 +0530

    Added :Remove Perticular Contact from AddressBook

--- JS/HomePage.js
+++ JS/HomePage.js
@@ -1,6 +1,6 @@
-let addresBookList
+let addressBookList
 window.addEventListener('DOMContentLoaded', (event) => {
-    addresBookList =   getAddressBookDataFromStorage();
+    addressBookList =   getAddressBookDataFromStorage();
     //document.querySelector(".emp-count").textContent = addresBookList.length;
     createInnerHtml();
 });
@@ -10,11 +10,11 @@ getAddressBookDataFromStorage= () => {
 }
 
 const createInnerHtml = () => {
-    if (addresBookList.length == 0) return;
+    if (addressBookList.length == 0) return;
     const headerHtml = "<tr><th>Full Name</th><th>Address</th><th>City</th><th>State</th>" +
         "<th>Zip Code</th><th>Phone Number</th><th>Actions</th></tr>";
     let innerHtml = `${headerHtml}`;
-    for (const addressBookData of addresBookList) {
+    for (const addressBookData of addressBookList) {
         innerHtml = `${innerHtml}
             <tr>
                
@@ -24,14 +24,26 @@ const createInnerHtml = () => {
                 <td>${addressBookData._state}</td>
                 <td>${addressBookData._zipCode}</td >
                 <td>${addressBookData._phoneNumber}</td>
-               
-
                 <td>
-                    <img id="1" onclick="remove(this)" alt="delete" src="/Assets/icons/delete-black-18dp.svg">
+                    <img id="${addressBookData._id}" onclick="remove(this)" alt="delete" src="/Assets/icons/delete-black-18dp.svg">
                     <img id="1" alt="edit" onclick="update(this)" src="/Assets/icons/create-black-18dp.svg">
                 </td>
             </tr>
         `;
     }
     document.querySelector('#display').innerHTML = innerHtml;
+}
+
+
+const remove = (node) => {
+    let addressBookData = addressBookList.find(bookData => bookData._id == node.id);
+
+    console.log(addressBookData)
+   if(!addressBookData) return;
+    const index = addressBookList 
+                    .map(addData => addData.id)
+                    .indexOf(addressBookData.id);
+                    addressBookList.splice(index, 1);
+    localStorage.setItem('AddressBookList', JSON.stringify(addressBookList));
+    createInnerHtml();
 }
